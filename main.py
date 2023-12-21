6ee83783cff4dc2769b097a32ac985f9d1b89f0c
Nikodem Wojtczak 247823 <247823@edu.p.lodz.pl>
2023-12-21 11:26:58 +0100

    21.12.2023

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import csv
-
 from backtracking_sudoku_generator import BackTrackingSudokuGenerator
 import random
 
 bsg = BackTrackingSudokuGenerator()
-
 
 def generate_100_sudoku_boards():
     list_of_sudoku = []
@@ -33,9 +31,10 @@ listOfSudokuMedium = generate_100_sudoku_boards()
 listOfSudokuHard = generate_100_sudoku_boards()
 
 for i in range(100):
-    listOfSudokuEasy[i] = erase_random_numbers(30, listOfSudokuEasy[i])
-    listOfSudokuMedium[i] = erase_random_numbers(47, listOfSudokuMedium[i])
-    listOfSudokuHard[i] = erase_random_numbers(60, listOfSudokuHard[i])
+    listOfSudokuEasy[i] = erase_random_numbers(45, listOfSudokuEasy[i])
+    listOfSudokuMedium[i] = erase_random_numbers(54, listOfSudokuMedium[i])
+    listOfSudokuHard[i] = erase_random_numbers(63, listOfSudokuHard[i])
+
 
 with open('listOfSudokuEasy.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
@@ -48,3 +47,5 @@ with open('listOfSudokuMedium.csv', 'w', newline='', encoding='utf-8') as file:
 with open('listOfSudokuHard.csv', 'w', newline='', encoding='utf-8',) as file:
     writer = csv.writer(file)
     writer.writerows(listOfSudokuHard)
+
+print("stop")
